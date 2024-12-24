4f6cc0da45d1cf834ea0f3f859b1187ef60a305e
Shivansh <shivanshmangla72@gmail.com>
2024-12-24 18:37:00 +0530

    Added global cart

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,13 +1,36 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/frontend_assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) =>{
 
-  const contextValue = {
-    food_list
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if(!cartItems[itemId]){
+      setCartItems((prev) => ({...prev, [itemId]:1}))
+    }
+    else{
+      setCartItems((prev) => ({...prev, [itemId]:prev[itemId]+1}))
+    }
   }
+
+  const removeFromCart = (itemId) =>{
+    setCartItems((prev) => ({...prev, [itemId]:prev[itemId]-1}))
+  }
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
+  }
+
+  useEffect(() =>{
+    console.log(cartItems)
+  }, [cartItems])
 
   return(
     <StoreContext.Provider value={contextValue}>
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/frontend_assets/assets'
+import { StoreContext } from '../../Context/StoreContext';
 
 const FoodItem = ({id, name, price, description, image}) => {
-
-
-  const [itemCount, setItemCount] = useState(0);
+  
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext)
 
   return (
     <div className='food-item'>
       <div className="food-item-image-container">
         <img src={image} alt="" className="food-item-image" />
         {
-        !itemCount
-          ?<img className='add' onClick={()=> setItemCount(prev=>prev+1)} src={assets.add_icon_white} />
+        !cartItems[id]
+          ?<img className='add' onClick={()=> addToCart(id)} src={assets.add_icon_white} />
           :<div className="food-item-counter">
-              <img onClick={()=> setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-              <p>{itemCount}</p>
-              <img onClick={()=> setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+              <img onClick={()=> removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+              <p>{cartItems[id]}</p>
+              <img onClick={()=> addToCart(id)} src={assets.add_icon_green} alt="" />
           </div>
         }
       </div>
