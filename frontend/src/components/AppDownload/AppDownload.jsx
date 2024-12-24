b5a31c7d37bdd5e535faa966c302b296459c53d3
Shivansh <shivanshmangla72@gmail.com>
2024-12-24 19:20:15 +0530

    Added navbar scroll

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AppDownload.css'
+
+const AppDownload = () => {
+  return (
+    <div className='app-download'>
+      App download
+    </div>
+  )
+}
+
+export default AppDownload
