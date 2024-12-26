7a72e68d23484980943560402ef1eee26920b0e7
Shivansh <shivanshmangla72@gmail.com>
2024-12-26 22:20:32 +0530

    Added List food items functionality

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -2,15 +2,63 @@ import React from 'react'
 import './Add.css'
 import { assets } from '../../assets/admin_assets/assets'
 import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
 
-const Add = () => {
+
+const Add = ({url}) => {
 
   const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Salad"
+  })
+
+
+  const onChangeHandler = (e) =>{
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setData(data=> ({...data, [name]: value}))
+  }
+
+  // useEffect(()=>{
+  //   console.log(data);
+  // }, [data])
+
+  const onSubmitHandler = async (e) =>{
+    e.preventDefault();
+    const formData = new FormData()
+    formData.append("name", data.name)
+    formData.append("description", data.description)
+    formData.append("price", Number(data.price))
+    formData.append("category", data.category)
+    formData.append("image", image)
+
+    const response = await axios.post(`${url}/api/food/add`, formData);
+
+    if (response.data.success){
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad"
+      });
+      setImage(false);
+      toast.success(response.data.message)
+    }
+    else{
+      toast.error(response.data.message)
+    }
+  }
 
 
   return (
     <div className='add'>
-      <form className='flex-col'>
+      <form className='flex-col' onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
@@ -20,16 +68,16 @@ const Add = () => {
         </div>
         <div className="add-product-name flex-col">
           <p>Product name</p>
-          <input type="text" placeholder='Type Here' name='name' />
+          <input onChange={onChangeHandler} value={data.name} type="text" placeholder='Type Here' name='name' />
         </div>
         <div className="add-product-description flex-col">
           <p>Product Description</p>
-          <textarea name="description" rows="6" placeholder='Write content here' required></textarea>
+          <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder='Write content here' required></textarea>
         </div>
         <div className="add-category-price">
           <div className="add-category flex-col">
             <p>Product Category</p>
-            <select name="category">
+            <select name="category" onChange={onChangeHandler} value={data.category}>
               <option value="Salad">Salad</option>
               <option value="Rolls">Rolls</option>
               <option value="Sandwich">Sandwich</option>
@@ -38,7 +86,7 @@ const Add = () => {
           </div>
           <div className="add-price flex-col">
             <p>Product Price</p>
-            <input type="number" name="price" placeholder='$20'/>
+            <input onChange={onChangeHandler} value={data.price} type="number" name="price" placeholder='$20'/>
           </div>
         </div>
         <button type='submit' className='add-button'>Add Item</button>
--- admin/src/pages/Lsit/List.jsx
+++ admin/src/pages/Lsit/List.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
 import './List.css'
+import { useState } from 'react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useEffect } from 'react'
 
-const List = () => {
+const List = ({url}) => {
+
+  const [list, setList] = useState([])
+
+  const fetchList = async ()=>{
+    const response = await axios.get(`${url}/api/food/list`)
+
+    if (response.data.success) {
+      setList(response.data.data)
+      toast.success(response.data.message)
+    }
+    else{
+      toast.error(response.data.message)
+    }
+  }
+
+  const removeFood = async(foodId)=>{
+    const response = await axios.post(`${url}/api/food/remove`, {id: foodId});
+    await fetchList();
+
+    if (response.data.success) {
+      toast.success(response.data.message)
+    }
+    else{
+      toast.error(response.data.message)
+    }
+  }
+
+  useEffect(()=>{
+    fetchList()
+  }, [])
+
   return (
-    <div>
-      
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Description</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index)=>{
+          return(
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/`+item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p>{item.description}</p>
+              <p className='cursor' onClick={()=>removeFood(item._id)}>X</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
