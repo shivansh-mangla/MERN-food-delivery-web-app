34421d017d55463befec78cfe9559e41df393a7a
Shivansh <shivanshmangla72@gmail.com>
2024-12-27 12:50:17 +0530

    cart integrated with mongo db

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/FoodRoute.js";
 import userRouter from "./routes/UserRoute.js";
 import "dotenv/config";
+import cartRouter from "./routes/CartRoute.js";
 
 
 //app config
@@ -22,6 +23,7 @@ connectDB()
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))  //exposing the 'uploads' folder on this API
 app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
 
 app.get("/", (req, res)=>{
   res.send("API is Working!!")
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -13,17 +13,29 @@ const StoreContextProvider = (props) =>{
 
   const [cartItems, setCartItems] = useState({});
 
-  const addToCart = (itemId) => {
+  const addToCart = async (itemId) => {
     if(!cartItems[itemId]){
       setCartItems((prev) => ({...prev, [itemId]:1}))
     }
     else{
       setCartItems((prev) => ({...prev, [itemId]:prev[itemId]+1}))
     }
+    if(token){
+      await axios.post(url+"/api/cart/add", {itemId}, {headers: {token}})
+    }
   }
 
-  const removeFromCart = (itemId) =>{
+  const removeFromCart = async (itemId) =>{
     setCartItems((prev) => ({...prev, [itemId]:prev[itemId]-1}))
+    if(token){
+      await axios.post(url+"/api/cart/remove", {itemId}, {headers: {token}})
+    }
+  }
+
+
+  const loadCartData = async (token) =>{
+    const response = await axios.post(url+"/api/cart/get", {}, {headers: {token}})
+    setCartItems(response.data.cartData)
   }
 
   const getTotalCartAmount = () =>{
@@ -48,6 +60,7 @@ const StoreContextProvider = (props) =>{
       await getFoodList()
       if(localStorage.getItem("token")){
         setToken(localStorage.getItem("token"))
+        await loadCartData(localStorage.getItem("token"))
       }
     }
     loadData()
